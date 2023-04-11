dd1be12ab461d540e57c80893d7cff4bff4bd63e
Jarrison <106404782+ElectricSheepStudios@users.noreply.github.com>
2023-04-11 20:24:09 +0100

    First Init
    INIT

---
+++ Source/DemoGame/DemoGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DemoGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMOGAME_API ADemoGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
